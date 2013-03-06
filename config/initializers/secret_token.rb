895034eea9f2db1da07402b5ebcb01379aed4309
mat <mat@mat-dev.(none)>
2013-03-05 21:16:34 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MatutterCom::Application.config.secret_token = 'b1bbe93a95b481f58556a59156254128216954ed82c733e7618a7854dbf97b4faaf470a9376d4d19425c8272051a288bc1f3836ba1ce06f6499e62561e2a42af'
