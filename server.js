6650a9aa69cfd6ef9de63c41319f54785baba437
Mat Utter <matutter4@gmail.com>
2013-12-24 18:18:07 -0500

    final changes for now, building a real app next

--- server.js
+++ server.js
@@ -11,8 +11,6 @@ function wake(route, handle) {
 }
 
 http.createServer(onRequest).listen(process.env.PORT || 8888);
-
-http.createServer(onRequest).listen(8888);
 console.log("Server "+status);
 }
 
